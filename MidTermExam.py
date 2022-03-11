1399b064eb187a85a29a52e60c4739c13ec50d53
AUSTIN <burkesean902@gmail.com>
2022-03-11 06:43:38 -0800

    Greeting and story

---
+++ MidTermExam.py
@@ -0,0 +1,4 @@
+
+print("Hello my name is Sean Burke I go To Gull Lake High School amd im in 9th grade")
+
+print(" Once unpon a time there was a guy who really wanted a burger but he Lived in LA and it was rush hour so the traffic was horrible. so then he decided to cut some traffic in line and when he did their was a CHP and he got pulled over and got a ticket fot failure to maintain lanes. ater the traffic stop he drove safely and got hiis burger 1 hour later")
